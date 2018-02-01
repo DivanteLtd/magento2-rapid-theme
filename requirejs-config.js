8e51b91a296c9c6b52778cc627a636a49a6c9899
Kacper Chara <kchara@divante.pl>
2018-02-01 16:13:02 +0100

    Remove: less colors local variables, slider, empty spaces in xml files, revert changes for copyright block

--- requirejs-config.js
+++ requirejs-config.js
@@ -1,21 +1,15 @@
 /**
  * @package Divante/rapid
  * @author Kacper Chara <kchara@divante.pl>
- * @copyright 2017 Divante Sp. z o.o.
+ * @copyright 2018 Divante Sp. z o.o.
  * @license See LICENSE_DIVANTE.txt for license details.
  */
 
 var config = {
     map: {
         '*': {
-            slick:                  'js/vendor/slick.min',
-            mCustomScrollbar:       'js/vendor/jquery.mCustomScrollbar.min',
-            mousewheel:             'js/vendor/jquery.mousewheel.min',
-            slider:                 'js/widgets/slider',
-            customScroll:           'js/widgets/custom-scroll',
             qtyInput:               'js/widgets/qty-input',
-            customCalendar:         'js/widgets/custom-calendar',
-            homePageSpecificJs:     'js/home-page-specific'
+            customCalendar:         'js/widgets/custom-calendar'
         }
     }
 };
